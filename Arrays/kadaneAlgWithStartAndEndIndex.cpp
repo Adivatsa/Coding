70e2ad01173bfa07779621b5336606fd304e3b6c
Aditya <chauhanaditya2411@gmail.com>
2022-04-02 00:27:15 +0530

    Kadane's Alg with Subarray Index

---
+++ Arrays/kadaneAlgWithStartAndEndIndex.cpp
@@ -0,0 +1,39 @@
+// C++ program to print largest contiguous array sum with start and End index
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int maxSubArraySum(int arr[], int size)
+{
+	int maxSum = INT_MIN, currSum = 0;
+    int s=0,e=0;
+
+	for (int i = 0; i < size; i++)
+	{
+		currSum = currSum + arr[i];
+
+        if(currSum<arr[i])//iske phle ka index ka sum not useful so start from this
+        {
+            currSum=arr[i];
+            s=i;
+        }
+
+        if(currSum>maxSum)//sum update ho rha ye last hga index
+        {
+            maxSum=currSum;
+            e=i;
+        }
+	}
+    cout<<" Start is "<<s<<" and End is "<<e<<endl;
+	return maxSum;
+}
+
+/*Driver program to test maxSubArraySum*/
+int main()
+{
+	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+	int n = sizeof(a)/sizeof(a[0]);
+	int max_sum = maxSubArraySum(a, n);
+	cout << "Maximum contiguous sum is " << max_sum;
+	return 0;
+}
