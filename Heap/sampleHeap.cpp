08cbae5ed81e24c71a9036682efbcdb962543d59
Aditya <chauhanaditya2411@gmail.com>
2022-06-01 11:04:05 +0530

    STL for Heap

---
+++ Heap/sampleHeap.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main()
+{
+
+    //Max Heap using STL
+    priority_queue<int> maxHeap;
+
+    //Min Heap
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    return 0;
+
+}
