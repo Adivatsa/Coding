af4007889fb1f91332bd825f51a952071deab107
Aditya <chauhanaditya2411@gmail.com>
2022-06-10 16:56:24 +0530

    Remove the Duplicates Elements from Sorted Array

---
+++ Arrays/removeDupSortedArray.cpp
@@ -0,0 +1,16 @@
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)
+            return 0;
+      
+        int j=0;
+        for(int i=j+1; i<n; i++)
+        {
+            if(nums[i]!=nums[j])
+                j++;
+            nums[j]=nums[i];
+            
+        }
+        
+       return j+1; 
+    }
