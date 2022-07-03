8507fb16212f0cda42e68460a670315488622bf3
Aditya Chauhan <38826506+Adivatsa@users.noreply.github.com>
2022-07-03 13:41:14 +0530

    Update celebrityProblem.cpp

--- stack/celebrityProblem.cpp
+++ stack/celebrityProblem.cpp
@@ -40,3 +40,44 @@
         
         return potentialCeleb;
     }
+
+
+//Little optimised
+
+int celebrity(vector<vector<int> >& M, int n) 
+    {
+        
+        stack<int> s;
+        //First push all into stack 
+        for(int i=0; i<n; i++)
+            s.push(i);
+            
+        //Jab tak one eemenrt not inside the stack
+        while(s.size()>1)
+        {
+            //dono elements ko nekal lo check kar lo ek dhushre ko jannete hai ya nai
+            int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+            if(M[a][b]==1)
+            {
+                //in this case a knows b so a is not celebrity as celebrity should know no one
+                s.push(b);
+            }
+            else
+                s.push(a);
+        }
+        
+        int ans=s.top();
+        //once check if all row are zero and all column should be one excluding that element
+        int zero=0;
+        for(int i=0 ; i<n; i++)
+        {
+            if(M[ans][i]!=0)
+                return -1;
+            if(M[i][ans]!=1 && i!=ans)
+                return -1;
+        }
+        return ans;
+    }
