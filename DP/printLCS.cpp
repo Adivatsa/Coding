5afaa9f74579c0df4bb213dfd864f0592cef8091
Aditya Chauhan <38826506+Adivatsa@users.noreply.github.com>
2022-07-16 14:24:59 +0530

    Update printLCS.cpp

--- DP/printLCS.cpp
+++ DP/printLCS.cpp
@@ -25,15 +25,31 @@ void printlcs(string x, string y, int m ,int n)
             }
         }
 
-        cout<<" LCS is  "<<t[m][n]<<endl;
-    for(int j=1; j<n+1; j++)
+        cout<<" LCS count is  "<<t[m][n]<<endl;
+    
+    string op="";
+    
+    int i=m;
+    int j=n;
+    while(i>0 && j>0)
     {
-        if(t[m][j]!= t[m][j-1])
-            cout<<y[j-1];
+        if(t[i-1]==t[j-1])
+        {
+            op=op+x[i-1];
+            i--;
+            j--;
+        }
+        else
+        {
+            if(t[i-1][j]>t[i][j-1]
+               i--;
+             else
+               j--;
+        }
     }
-
-
+    cout<<op<<endl;
 }
+               
 int main()
 {
 
