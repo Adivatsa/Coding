d12df23aa54ed5fa334a6fcbaac278676a7d701b
Aditya Chauhan <38826506+Adivatsa@users.noreply.github.com>
2022-07-20 13:39:57 +0530

    Update editDistance.cpp

--- DP/editDistance.cpp
+++ DP/editDistance.cpp
@@ -66,4 +66,26 @@
             }
         }
         return t[n][m];
+        
+        //Space Optimization
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+
+        for(int j=0; j<m+1; j++)
+            {
+                prev[j]=j;
+            }
+        
+        for(int i=1; i<n+1; i++)
+        {
+            curr[0]=i;
+            for(int j=1; j<m+1; j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                    curr[j]= prev[j-1];
+                else
+                    curr[j]= 1+ min(curr[j-1], min(prev[j-1], prev[j]));
+            }
+            prev=curr;
+        }
+        return prev[m];
     }
